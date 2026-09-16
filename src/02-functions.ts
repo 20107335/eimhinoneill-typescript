f7801d3acbe1369184cbb1cc7f0080ddb25ea181
20107335 <20107335@mail.wit.ie>
2026-09-16 21:07:44 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -25,6 +25,17 @@ function addColleague(colleagues: Colleague[], name: string, department: string,
   });
 }
 
+function addInterest(friend: Friend, interest: string) {
+  if (!friend.interests) {
+    friend.interests = [];
+  }
+  friend.interests.push(interest);
+  return friend.interests;
+}
+
+friends[0].interests = ['Music', 'Sport'];
+console.log(addInterest(friends[0], 'Politics'));
+
 function sortColleagues(
   colleagues: Colleague[],
   sorter: (c1: Colleague, c2: Colleague) => number,
@@ -38,12 +49,10 @@ function sortColleagues(
   const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
   return fullResult.slice(0,end)
 }
-// Test invocations
+
 console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
-
-
 
 function findFriends(friends: Friend[], criterion: (f: Friend) => boolean) {
   return friends.filter(criterion).map((f) => f.name);
